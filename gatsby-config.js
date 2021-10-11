eb70a10f5336fe530a79a4207c5d22497a19e368
m-zs <mzasada93@gmail.com>
2021-10-11 21:22:59 +0200

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "we-develop",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'we-develop'
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/'
       },
-      __key: "pages",
-    },
-  ],
-};
+      __key: 'pages'
+    }
+  ]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return <main></main>
+}
+
+export default IndexPage
