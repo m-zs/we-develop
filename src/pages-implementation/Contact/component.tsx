5902554d69f9d38d1fe36759a0092bc059c70b36
m-zs <mzasada93@gmail.com>
2021-10-26 16:07:25 +0200

    feat: add contact page

---
+++ src/pages-implementation/Contact/component.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import * as S from './style';
+import Seo from 'components/Seo';
+import HighlightedLink from 'components/HighlightedLink';
+
+const Contact = () => (
+  <>
+    <Seo title="Contact" description="Get in touch." />
+
+    <S.Introduction>
+      You can contact me by{' '}
+      <HighlightedLink
+        href="mailto:mzasada93@gmail.com"
+        text="Email"
+        type="a"
+      />
+      , take a look at my private{' '}
+      <HighlightedLink href="https://github.com/m-zs/" text="GitHub" type="a" />{' '}
+      or add at{' '}
+      <HighlightedLink
+        href="https://www.linkedin.com/in/mateusz-zasada-329176117"
+        text="LinkedIn"
+        type="a"
+      />
+    </S.Introduction>
+  </>
+);
+
+export default Contact;
